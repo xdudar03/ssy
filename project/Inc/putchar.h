fbd3f22f82cf2223a90630fd0027540876921ec2
Oleksandra Dudar <99721956+xdudar03@users.noreply.github.com>
2026-03-24 10:07:32 +0100

    project eth

---
+++ project/Inc/putchar.h
@@ -0,0 +1,15 @@
+/*
+ * putchar.h
+ *
+ *  Created on: 10. 2. 2026
+ *      Author: Student
+ */
+
+#ifndef INC_PUTCHAR_H_
+#define INC_PUTCHAR_H_
+
+#include "main.h"
+
+#define PUTCHAR_PROTOTYPE int __io_putchar (int ch)
+
+#endif /* INC_PUTCHAR_H_ */
